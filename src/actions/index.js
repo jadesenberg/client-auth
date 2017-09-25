72909b9b22aaaa9b4e18a4ecb07603bd622ad04c
Jade Bautista <edaj.bautista@gmail.com>
2017-09-25 23:15:41 +0800

    Setup and initialize redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 
 export function signinUser({ email, passowrd }) {
-    
+    return function(dispatch) { //way to connect to dispatch and reducers..
+        // code here
+    }
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
 import Signin from './components/auth/signin';
 import Header from './components/header';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
