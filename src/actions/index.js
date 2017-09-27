4149842f222a8a15b92528724017cc5fb8904204
Jade Bautista <edaj.bautista@gmail.com>
2017-09-27 23:34:36 +0800

    Print email

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,6 @@
 export function signinUser({ email, passowrd }) {
     return function(dispatch) { //way to connect to dispatch and reducers..
         // code here
+        console.log(email);
     }
 }
